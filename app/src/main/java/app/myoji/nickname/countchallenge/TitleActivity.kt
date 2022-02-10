da0f69f72fc7ccfdc9ad9d0dfea38b0f2b5b1b74
mona-apk <cordion_nap@yahoo.co.jp>
2022-02-11 07:41:51 +0900

    ViewBindingに置き換え

--- app/src/main/java/app/myoji/nickname/countchallenge/TitleActivity.kt
+++ app/src/main/java/app/myoji/nickname/countchallenge/TitleActivity.kt
@@ -2,10 +2,14 @@ package app.myoji.nickname.countchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.myoji.nickname.countchallenge.databinding.ActivityTitleBinding
 
 class TitleActivity : AppCompatActivity() {
+    // バインディングクラスの変数
+    private lateinit var binding: ActivityTitleBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_title)
+        binding = ActivityTitleBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
