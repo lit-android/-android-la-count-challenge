9299bd9ef2d1ce1684ed0597291467aee8d3a0fd
mona-apk <cordion_nap@yahoo.co.jp>
2022-02-11 07:33:49 +0900

    コメントの追加と定数化

--- app/src/main/java/app/myoji/nickname/countchallenge/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/countchallenge/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var soundPool: SoundPool
     private var sound: Int = 0
 
+    // SharedPreferencesの変数
     private lateinit var prefs: SharedPreferences
 
     // カウント変数
@@ -30,8 +31,9 @@ class MainActivity : AppCompatActivity() {
         sound = soundPool.load(this, R.raw.sound, 1)
 
         // SharedPreferencesの初期化
-        prefs = getSharedPreferences("CountChallenge", Context.MODE_PRIVATE)
-        count = prefs.getInt("Count", 0)
+        prefs = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+        // カウント数の読み込み
+        count = prefs.getInt(COUNT_KEY, 0)
 
         setCountText()
 
@@ -44,7 +46,7 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         // カウント数の保存
         val editor = prefs.edit()
-        editor.putInt("count", count)
+        editor.putInt(COUNT_KEY, count)
         editor.apply()
 
         super.onPause()
@@ -60,6 +62,11 @@ class MainActivity : AppCompatActivity() {
                 binding.countText.setTextColor(Color.BLUE)
                 soundPool.play(sound, 1.0f, 1.0f, 0, 0, 1.0f)
             }
-         }
+        }
+    }
+
+    companion object {
+        const val SHARED_PREFERENCES_NAME = "CountChallenge"
+        const val COUNT_KEY = "count"
     }
 }
