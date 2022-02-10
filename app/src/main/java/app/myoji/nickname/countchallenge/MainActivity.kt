6a643ff78fb3461c6504d1fc9ca86c2d14c76574
mona-apk <cordion_nap@yahoo.co.jp>
2022-02-11 06:11:34 +0900

    ViewBinding変数の設定

--- app/src/main/java/app/myoji/nickname/countchallenge/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/countchallenge/MainActivity.kt
@@ -2,10 +2,14 @@ package app.myoji.nickname.countchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.myoji.nickname.countchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // バインディングクラスの変数
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
