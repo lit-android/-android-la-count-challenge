9dab7d9552eb6276e185dea687f440d3690e83c4
mona-apk <cordion_nap@yahoo.co.jp>
2022-02-11 07:15:32 +0900

    奇数のタイミングで音源を鳴らす

--- app/src/main/java/app/myoji/nickname/countchallenge/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/countchallenge/MainActivity.kt
@@ -3,6 +3,8 @@ package app.myoji.nickname.countchallenge
 import android.content.Context
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.myoji.nickname.countchallenge.databinding.ActivityMainBinding
@@ -16,6 +18,10 @@ class MainActivity : AppCompatActivity() {
     // カウント変数
     private var count = 0
 
+    // SoundPoolの変数
+    private lateinit var soundPool: SoundPool
+    private var sound: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
@@ -23,6 +29,10 @@ class MainActivity : AppCompatActivity() {
         // SharedPreferencesの初期化
         prefs = getSharedPreferences("CountChallenge", Context.MODE_PRIVATE)
         count = prefs.getInt("Count", 0)
+
+        // SoundPoolの設定
+        soundPool = SoundPool.Builder().build()
+        sound = soundPool.load(this, R.raw.sound, 1)
 
         setCountText()
 
@@ -40,6 +50,7 @@ class MainActivity : AppCompatActivity() {
             }
             1 -> {
                 binding.countText.setTextColor(Color.BLUE)
+                soundPool.play(sound, 1.0f, 1.0f, 0, 0, 1.0f)
             }
          }
     }
