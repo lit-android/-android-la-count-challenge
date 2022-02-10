122cb8c828e93a2ccb1852105f092e6daa5c219b
mona-apk <cordion_nap@yahoo.co.jp>
2022-02-11 06:22:15 +0900

    カウントアプリの実装

--- app/src/main/java/app/myoji/nickname/countchallenge/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/countchallenge/MainActivity.kt
@@ -8,8 +8,18 @@ class MainActivity : AppCompatActivity() {
     // バインディングクラスの変数
     private lateinit var binding: ActivityMainBinding
 
+    // カウント変数
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        binding.countText.text = count.toString()
+
+        binding.upButton.setOnClickListener {
+            count++
+            binding.countText.text = count.toString()
+        }
     }
 }
