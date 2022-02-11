d14937c167043e74c39fe317b079cc5ead0089bc
mona-apk <cordion_nap@yahoo.co.jp>
2022-02-11 18:32:05 +0900

    チュートリアル画面の作成

--- app/src/main/java/app/myoji/nickname/countchallenge/ThirdFragment.kt
+++ app/src/main/java/app/myoji/nickname/countchallenge/ThirdFragment.kt
@@ -1,16 +1,55 @@
 package app.myoji.nickname.countchallenge
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import app.myoji.nickname.countchallenge.databinding.FragmentThirdBinding
 
 class ThirdFragment : Fragment() {
+    // クリックリスナー
+    interface ClickListener {
+        fun onClickButton()
+    }
+
+    // バインディング変数
+    private var _binding: FragmentThirdBinding? = null
+    private val binding get() = _binding!!
+    private var listener: ClickListener? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_third, container, false)
+    ): View {
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            listener?.onClickButton()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as? ClickListener
+        if (listener == null) {
+            throw ClassCastException("$context must implement ClickListener")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
     }
 }
--- app/src/main/java/app/myoji/nickname/countchallenge/TutorialActivity.kt
+++ app/src/main/java/app/myoji/nickname/countchallenge/TutorialActivity.kt
@@ -1,17 +1,52 @@
 package app.myoji.nickname.countchallenge
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import app.myoji.nickname.countchallenge.databinding.ActivityTutorialBinding
 
-class TutorialActivity : AppCompatActivity() {
+class TutorialActivity : AppCompatActivity(), ThirdFragment.ClickListener {
     // バインディング変数
-
     private lateinit var binding: ActivityTutorialBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =
             ActivityTutorialBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        binding.viewPager.adapter = SlidePagerAdapter(this)
+        binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+
+    inner class SlidePagerAdapter(fragmentActivity: FragmentActivity) :
+        FragmentStateAdapter(fragmentActivity) {
+
+        override fun getItemCount(): Int = 3
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> FirstFragment()
+                1 -> SecondFragment()
+                2 -> ThirdFragment()
+                else -> throw Exception()
+            }
+        }
+    }
+
+    override fun onClickButton() {
+        val toTitleActivityIntent = Intent(this, TitleActivity::class.java)
+        startActivity(toTitleActivityIntent)
+        finish()
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, TutorialActivity::class.java)
+        }
     }
 }
